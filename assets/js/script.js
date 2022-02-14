35e0e619dca48b24982314af2806e1e007564b5e
Mustafa Ansari <mustwoah@gmail.com>
2022-02-14 04:51:25 +0330

    x

---
+++ assets/js/script.js
@@ -0,0 +1,125 @@
+
+$( document ).ready(function() {
+
+  setTimeout(showlogo, 500);
+  setTimeout(showSecondLine, 800);
+  setTimeout(showThirdLine, 1200);
+  setTimeout(showGrayBox, 1250);
+  setTimeout(showFirsticon, 1250);
+  setTimeout(showSecondicon, 1250);
+  setTimeout(showThirdicon, 1250);
+  setTimeout(showFourthicon, 1250);
+  setTimeout(showFifthicon, 1250);
+  setTimeout(showSixthicon, 1250);
+setTimeout(showGrayBox2, 1250);
+  setTimeout(showFirsticon2, 1250);
+  setTimeout(showSecondicon2, 1250);
+  setTimeout(showThirdicon2, 1250);
+  setTimeout(showFourthicon2, 1250);
+  setTimeout(showFifthicon2, 1250);
+  setTimeout(showSixthicon2, 1250);
+    setTimeout(showGrayBox3, 1250);
+  setTimeout(showFirsticon3, 1250);
+  setTimeout(showSecondicon3, 1250);
+  setTimeout(showThirdicon3, 1250);
+  setTimeout(showFourthicon3, 1250);
+  setTimeout(showFifthicon3, 1250);
+  setTimeout(showSixthicon3, 1250);
+
+
+});
+
+function showlogo() {
+   $('#logo').addClass('visible');
+}
+
+function showSecondLine() {
+   $('#second-line').addClass('visible');
+}
+
+function showThirdLine() {
+   $('#third-line').addClass('visible');
+}
+
+function showGrayBox() {
+   $('#gray-box').addClass('visible');
+}
+
+function showFirsticon() {
+   $('#first-icon').addClass('visible-icon');
+}
+
+function showSecondicon() {
+   $('#second-icon').addClass('visible-icon');
+}
+
+function showThirdicon() {
+   $('#third-icon').addClass('visible-icon');
+}
+
+function showFourthicon() {
+   $('#fourth-icon').addClass('visible-icon');
+}
+
+function showFifthicon() {
+   $('#fifth-icon').addClass('visible-icon');
+}
+
+function showSixthicon() {
+   $('#sixth-icon').addClass('visible-icon');
+}
+
+function showGrayBox2() {
+   $('#gray-box2').addClass('visible');
+}
+
+function showFirsticon2() {
+   $('#first-icon2').addClass('visible-icon');
+}
+
+function showSecondicon2() {
+   $('#second-icon2').addClass('visible-icon');
+}
+
+function showThirdicon2() {
+   $('#third-icon2').addClass('visible-icon');
+}
+
+function showFourthicon2() {
+   $('#fourth-icon2').addClass('visible-icon');
+}
+
+function showFifthicon2() {
+   $('#fifth-icon2').addClass('visible-icon');
+}
+
+function showSixthicon2() {
+   $('#sixth-icon2').addClass('visible-icon');
+}
+function showGrayBox3() {
+   $('#gray-box3').addClass('visible');
+}
+
+function showFirsticon3() {
+   $('#first-icon3').addClass('visible-icon');
+}
+
+function showSecondicon3() {
+   $('#second-icon3').addClass('visible-icon');
+}
+
+function showThirdicon3() {
+   $('#third-icon3').addClass('visible-icon');
+}
+
+function showFourthicon3() {
+   $('#fourth-icon3').addClass('visible-icon');
+}
+
+function showFifthicon3() {
+   $('#fifth-icon3').addClass('visible-icon');
+}
+
+function showSixthicon3() {
+   $('#sixth-icon3').addClass('visible-icon');
+}
